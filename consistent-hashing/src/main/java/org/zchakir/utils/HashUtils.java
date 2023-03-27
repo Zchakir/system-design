7091b8e3841f023e67456c0882597a9d7b21b57e
Zouhair CHAKIR <zchakir11@gmail.com>
2023-03-27 13:12:01 +0000

    Revert "Revert "Load Balancer implementation and Test""
    This reverts commit 6204d60bbf2b5993ece16a045953fb7adf28fd1a.

--- consistent-hashing/src/main/java/org/zchakir/utils/HashUtils.java
+++ consistent-hashing/src/main/java/org/zchakir/utils/HashUtils.java
@@ -4,11 +4,18 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class HashUtils {
-    public static long getHash(String key) throws NoSuchAlgorithmException {
+    public long getHash(String key) {
         // Use a hash function to map the key to a 64-bit integer
         // For example, use the MD5 hash algorithm
-        MessageDigest md = MessageDigest.getInstance("MD5");
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            // Work on more general exception message
+            System.out.println("Message Digest can't get the md5 algorithm");
+        }
         byte[] digest = md.digest(key.getBytes());
+        // To be refactored
         long h = ((long)digest[0] << 56) |
                 ((long)(digest[1] & 255) << 48) |
                 ((long)(digest[2] & 255) << 40) |
