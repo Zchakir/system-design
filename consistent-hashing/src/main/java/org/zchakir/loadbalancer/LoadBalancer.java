6204d60bbf2b5993ece16a045953fb7adf28fd1a
Zouhair CHAKIR <zchakir11@gmail.com>
2023-03-27 13:11:01 +0000

    Revert "Load Balancer implementation and Test"
    This reverts commit 0933685ce39cca79c26d4ffaa7cbfdd81d3b0958.

--- consistent-hashing/src/main/java/org/zchakir/loadbalancer/LoadBalancer.java
+++ consistent-hashing/src/main/java/org/zchakir/loadbalancer/LoadBalancer.java
@@ -1,57 +1,25 @@
 package org.zchakir.loadbalancer;
-
-
-import org.zchakir.utils.HashUtils;
-
-import java.util.*;
-
+import java.util.List;
+import java.util.TreeMap;
 public class LoadBalancer {
+
     private TreeMap<Long, String> serverHashes = new TreeMap<>();
     private List<String> serverPool;
 
     public LoadBalancer(List<String> serverPool) {
-        this.serverPool = serverPool;
-        for (String server : serverPool) {
-            addServer(server);
-        }
+
     }
 
     public void addServer(String server) {
-        // Add the server to the server pool
-        serverPool.add(server);
 
-        // Generate server hashes
-        for (int i = 0; i < 100; i++) {
-            String virtualServer = server + "-" + i;
-            long hash = new HashUtils().getHash(virtualServer);
-            serverHashes.put(hash, server);
-        }
     }
 
     public void removeServer(String server) {
-        // Remove the server from the server pool
-        serverPool.remove(server);
 
-        // Remove server hashes
-        Iterator<Map.Entry<Long, String>> iterator = serverHashes.entrySet().iterator();
-        while (iterator.hasNext()) {
-            Map.Entry<Long, String> entry = iterator.next();
-            if (entry.getValue().equals(server)) {
-                iterator.remove();
-            }
-        }
     }
 
     public String getServer(String key) {
-        // Get the hash of the key
-        long hash = new HashUtils().getHash(key);
-
-        // Get the server with the smallest hash greater than or equal to the key hash
-        SortedMap<Long, String> tailMap = serverHashes.tailMap(hash);
-        if (tailMap.isEmpty()) {
-            return serverHashes.firstEntry().getValue();
-        }
-        return tailMap.get(tailMap.firstKey());
+        return "";
     }
 }
 
--- consistent-hashing/src/main/java/org/zchakir/utils/HashUtils.java
+++ consistent-hashing/src/main/java/org/zchakir/utils/HashUtils.java
@@ -4,18 +4,11 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class HashUtils {
-    public long getHash(String key) {
+    public static long getHash(String key) throws NoSuchAlgorithmException {
         // Use a hash function to map the key to a 64-bit integer
         // For example, use the MD5 hash algorithm
-        MessageDigest md = null;
-        try {
-            md = MessageDigest.getInstance("MD5");
-        } catch (NoSuchAlgorithmException e) {
-            // Work on more general exception message
-            System.out.println("Message Digest can't get the md5 algorithm");
-        }
+        MessageDigest md = MessageDigest.getInstance("MD5");
         byte[] digest = md.digest(key.getBytes());
-        // To be refactored
         long h = ((long)digest[0] << 56) |
                 ((long)(digest[1] & 255) << 48) |
                 ((long)(digest[2] & 255) << 40) |
